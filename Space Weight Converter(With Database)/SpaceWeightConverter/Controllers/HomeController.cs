4ac6fc824c86094a5b86a3f19f3b131b7f8d3fb0
Stanimir_Stoev <st100ev@abv.bg>
2021-03-17 15:50:38 +0200

    Add files via upload

---
+++ Space Weight Converter(With Database)/SpaceWeightConverter/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using SpaceWeightConverter.Models;
+
+
+namespace SpaceWeightConverter.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(Calculation calculator)
+        {
+            return View(calculator);
+        }
+
+        [HttpPost]
+        public IActionResult Calculate(Calculation calculator)
+        {
+            SqlConnectionStringBuilder con = new SqlConnectionStringBuilder();
+            string connectionString = @"Data Source = USER-PC\SQLEXPRESS01; Initial Catalog = SpaceWeightConverterDb; Integrated Security = True";
+
+            string queryString = "select Coefficient from DataFromCalculate where SpaceObject = @name";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand command = connection.CreateCommand())
+            {
+                command.CommandText = queryString;
+                command.Parameters.AddWithValue("@name", calculator.SpaceObjectName);
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        calculator.CoefficientValue = (decimal)reader["Coefficient"];
+                        calculator.CalculateResult();
+                    }
+                }
+            }
+
+            return RedirectToAction("Index", calculator);
+        }
+    }
+}
+
---
+++ Space Weight Converter(With Database)/SpaceWeightConverter/Models/Calculation.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpaceWeightConverter.Models
+{
+    public class Calculation
+    {
+        public Calculation()
+        {
+            this.Result = 0;         
+        }
+
+        [Key]
+        public int ID { get; set; }
+        public string SpaceObjectName { get; set; }
+        public decimal Weight { get; set; }
+        public string Operator { get; set; }
+        public decimal Result { get; set; }
+        public decimal CoefficientValue { get; set; }
+       
+        public void CalculateResult()
+        {
+            this.Result = this.Weight * this.CoefficientValue;
+        }
+    }
+}
+
+
